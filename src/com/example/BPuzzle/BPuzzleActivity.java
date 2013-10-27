934abfe1216b1a29370420b0832a2ee8498e778a
konnigud <konnigud@gmail.com>
2013-10-27 20:13:17 +0000

    Adding src

---
+++ src/com/example/BPuzzle/BPuzzleActivity.java
@@ -0,0 +1,28 @@
+package com.example.BPuzzle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konráð
+ * Date: 27.10.2013
+ * Time: 19:39
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class BPuzzleActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void buttonPressed( View view){
+        Intent intent = new Intent(this, GameActivity.class);
+    }
+}
---
+++ src/com/example/BPuzzle/GameActivity.java
@@ -0,0 +1,11 @@
+package com.example.BPuzzle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konráð
+ * Date: 27.10.2013
+ * Time: 19:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameActivity {
+}
